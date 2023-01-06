abe19fc7d97bdc17119518fbd052f377d520d3a0
Daniel Buenrrostro <Daniel@DanielBuenrrostro.com>
2023-01-06 09:53:34 -0600

    Got strings with length 5 to pass

--- challenges/Module A - Inside-Out (classic-stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
+++ challenges/Module A - Inside-Out (classic-stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
@@ -1,5 +1,10 @@
+import { isPasswordValid } from "./index";
 
 describe('password validator', () => {
-
+    describe('Between 5 and 15 characters long', () => {
+        it('returns true for strings with 5 characters', () => {
+            expect(isPasswordValid('12345')).toBeTruthy();
+        })
+    })
 })
 
