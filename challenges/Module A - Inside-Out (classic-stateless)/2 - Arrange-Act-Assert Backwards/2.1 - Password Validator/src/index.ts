18c25efb0351dc6052834c9ae29977fe5ab226be
Daniel Buenrrostro <Daniel@DanielBuenrrostro.com>
2023-01-06 10:19:05 -0600

    Refactored boolean setting so that it doesnt compute validity twice

--- challenges/Module A - Inside-Out (classic-stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.ts
+++ challenges/Module A - Inside-Out (classic-stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.ts
@@ -5,25 +5,26 @@ interface PasswordValidityResult {
 
 export function isPasswordValid(str: string): PasswordValidityResult {
     let errors = [];
-    const valid = str.length >= 5 &&
-        str.length <= 15 &&
-        /[A-Z]/.test(str) &&
-        /[0-9]/.test(str);
+    let valid = true;
 
     if (!(str.length >= 5)) {
         errors.push('Password must be at least 5 characters');
+        valid = false;
     }
 
     if (!(str.length <= 15)) {
         errors.push('Password must have more than 15 characters');
+        valid = false;
     }
 
     if (!(/[A-Z]/.test(str))) {
         errors.push('Password must contain at least one upper case letter');
+        valid = false;
     }
 
     if (!(/[0-9]/.test(str))) {
         errors.push('Password must contain at least one number');
+        valid = false;
     }
 
     return { valid, errors };
