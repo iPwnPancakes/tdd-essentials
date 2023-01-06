70492e3ba2bbeb641a0293a4b9a96b4d6b8cdb89
Daniel Buenrrostro <Daniel@DanielBuenrrostro.com>
2023-01-06 10:09:53 -0600

    Refactored return type to object

--- challenges/Module A - Inside-Out (classic-stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
+++ challenges/Module A - Inside-Out (classic-stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
@@ -3,33 +3,39 @@ import { isPasswordValid } from "./index";
 describe('password validator', () => {
     describe('Between 5 and 15 characters long', () => {
         it('returns true for strings with 5 characters', () => {
-            expect(isPasswordValid('12345A')).toBeTruthy();
+            expect(isPasswordValid('12345A').valid).toBeTruthy();
         })
 
         it('returns true for strings with 15 characters', () => {
-            expect(isPasswordValid('123456789ABCDEF')).toBeTruthy();
+            expect(isPasswordValid('123456789ABCDEF').valid).toBeTruthy();
         })
 
         it('returns false for strings with 1 character', () => {
-            expect(isPasswordValid('1')).toBeFalsy();
+            expect(isPasswordValid('1').valid).toBeFalsy();
         })
 
         it('returns false for strings with 16 characters', () => {
-            expect(isPasswordValid('123456789ABCDEFF')).toBeFalsy();
+            expect(isPasswordValid('123456789ABCDEFF').valid).toBeFalsy();
         })
     })
 
     describe('Contains at least one digit Contains at least one upper case letter', () => {
         it('should return true for 123456789ABCDEF', () => {
-            expect(isPasswordValid('123456789ABCDEF')).toBeTruthy();
+            expect(isPasswordValid('123456789ABCDEF').valid).toBeTruthy();
         })
 
         it('should return false if no upper case letters in string', () => {
-            expect(isPasswordValid('123456789abcdef')).toBeFalsy();
+            expect(isPasswordValid('123456789abcdef').valid).toBeFalsy();
         })
 
         it('should return false if no digits in string', () => {
-            expect(isPasswordValid('ABCDEFG')).toBeFalsy();
+            expect(isPasswordValid('ABCDEFG').valid).toBeFalsy();
+        })
+    })
+
+    describe('Return an object containing a boolean result and an errors key that contains an error message or type for all errors in occurrence', () => {
+        it('should return an object', () => {
+            expect(typeof isPasswordValid('ABCDEFG')).toBe('object');
         })
     })
 })
--- challenges/Module A - Inside-Out (classic-stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.ts
+++ challenges/Module A - Inside-Out (classic-stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.ts
@@ -1,6 +1,13 @@
-export function isPasswordValid(str: string): boolean {
-    return str.length >= 5 &&
+interface PasswordValidityResult {
+    valid: boolean,
+    errors: Array<string>
+}
+
+export function isPasswordValid(str: string): PasswordValidityResult {
+    const valid = str.length >= 5 &&
         str.length <= 15 &&
         /[A-Z]/.test(str) &&
         /[0-9]/.test(str);
+
+    return { valid, errors: [] };
 }
