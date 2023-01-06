35d00ec25eda488bdb42813c87d93d5d804c94ee
Daniel Buenrrostro <Daniel@DanielBuenrrostro.com>
2023-01-06 10:03:14 -0600

    Now enforcing one upper case letter

--- challenges/Module A - Inside-Out (classic-stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
+++ challenges/Module A - Inside-Out (classic-stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
@@ -3,7 +3,7 @@ import { isPasswordValid } from "./index";
 describe('password validator', () => {
     describe('Between 5 and 15 characters long', () => {
         it('returns true for strings with 5 characters', () => {
-            expect(isPasswordValid('12345')).toBeTruthy();
+            expect(isPasswordValid('12345A')).toBeTruthy();
         })
 
         it('returns true for strings with 15 characters', () => {
@@ -16,6 +16,16 @@ describe('password validator', () => {
 
         it('returns false for strings with 16 characters', () => {
             expect(isPasswordValid('123456789ABCDEFF')).toBeFalsy();
+        })
+    })
+
+    describe('Contains at least one digit Contains at least one upper case letter', () => {
+        it('should return true for 123456789ABCDEF', () => {
+            expect(isPasswordValid('123456789ABCDEF')).toBeTruthy();
+        })
+
+        it('should return false for 123456789abcdef', () => {
+            expect(isPasswordValid('123456789abcdef')).toBeFalsy();
         })
     })
 })
--- challenges/Module A - Inside-Out (classic-stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.ts
+++ challenges/Module A - Inside-Out (classic-stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.ts
@@ -1,3 +1,5 @@
 export function isPasswordValid(str: string): boolean {
-    return str.length >= 5 && str.length <= 15;
+    return str.length >= 5 &&
+        str.length <= 15 &&
+        /[A-Z]/.test(str);
 }
