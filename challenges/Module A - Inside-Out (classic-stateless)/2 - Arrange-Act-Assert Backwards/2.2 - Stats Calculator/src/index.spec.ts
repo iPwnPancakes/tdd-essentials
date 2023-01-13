de041c157e27d9d81f5788b84ff699ff049bf632
Daniel Buenrrostro <Daniel@DanielBuenrrostro.com>
2023-01-13 15:35:51 -0600

    Wrote failing test for finding max value

--- challenges/Module A - Inside-Out (classic-stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
+++ challenges/Module A - Inside-Out (classic-stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
@@ -14,4 +14,12 @@ describe('stats calculator', () => {
             expect(computeStats(numbers).min).toBe(2);
         })
     })
+
+    describe('maximum value', () => {
+        it('returns 53 given the values [2, 4, 21, -8, 53, 40]', () => {
+            const numbers = [2, 4, 21, -8, 53, 40];
+
+            expect(computeStats(numbers).max).toBe(53);
+        })
+    })
 })
