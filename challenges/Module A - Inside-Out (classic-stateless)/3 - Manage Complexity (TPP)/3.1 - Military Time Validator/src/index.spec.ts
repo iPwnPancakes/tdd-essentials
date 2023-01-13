207112429d55dd0f163a43ea4cf4ab0c7624eb71
Daniel Buenrrostro <Daniel@DanielBuenrrostro.com>
2023-01-13 15:55:41 -0600

    Created initial failing test that focuses on the hours only

--- challenges/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.1 - Military Time Validator/src/index.spec.ts
+++ challenges/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.1 - Military Time Validator/src/index.spec.ts
@@ -1,5 +1,11 @@
+import { isValidRange } from "./index";
 
 describe('military time validator', () => {
+    describe('hours only', () => {
+        it('returns true if given the range: 00:00 - 24:00', () => {
+            const input = '00:00 - 24:00';
 
-
+            expect(isValidRange(input)).toBeTruthy();
+        })
+    })
 })
