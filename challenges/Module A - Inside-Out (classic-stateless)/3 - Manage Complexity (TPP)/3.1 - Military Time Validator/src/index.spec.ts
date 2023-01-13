c9b2a0d1d3414f345038c5d29caf836529b6c8e4
Daniel Buenrrostro <Daniel@DanielBuenrrostro.com>
2023-01-13 16:12:17 -0600

    Got second test to pass, accidentally implemented the entire thing

--- challenges/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.1 - Military Time Validator/src/index.spec.ts
+++ challenges/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.1 - Military Time Validator/src/index.spec.ts
@@ -1,17 +1,21 @@
 import { isValidRange } from "./index";
 
 describe('military time validator', () => {
-    describe('hours only', () => {
-        it('returns true if given the range: 00:00 - 24:00', () => {
-            const input = '00:00 - 24:00';
+    it('returns true if given the range: 00:00 - 24:00', () => {
+        const input = '00:00 - 24:00';
 
-            expect(isValidRange(input)).toBeTruthy();
-        })
+        expect(isValidRange(input)).toBeTruthy();
+    })
 
-        it('returns false if given the range: 00:00 - 25:00', () => {
-            const input = '00:00 - 25:00';
+    it('returns false if given the range: 00:00 - 25:00', () => {
+        const input = '00:00 - 25:00';
 
-            expect(isValidRange(input)).toBeFalsy();
-        })
+        expect(isValidRange(input)).toBeFalsy();
+    })
+
+    it('returns true if given the range 01:12 - 14:32', () => {
+        const input = '01:12 - 14:32';
+
+        expect(isValidRange(input)).toBeTruthy();
     })
 })
--- challenges/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.1 - Military Time Validator/src/index.ts
+++ challenges/Module A - Inside-Out (classic-stateless)/3 - Manage Complexity (TPP)/3.1 - Military Time Validator/src/index.ts
@@ -1,3 +1,41 @@
 export function isValidRange(str: string): boolean {
-    return true;
+    const timeEntries = breakStringIntoEntries(str);
+    if (timeEntries.length !== 2) {
+        return false;
+    }
+
+    const firstEntry = timeEntries[0];
+    const secondEntry = timeEntries[1];
+
+    return isValidMilitaryTime(firstEntry) && isValidMilitaryTime(secondEntry);
 }
+
+function breakStringIntoEntries(str: string): string[] {
+    const tokens = str.split(' - ');
+    if (tokens.length < 2) {
+        return [];
+    }
+
+    return tokens;
+}
+
+function isValidMilitaryTime(str: string): boolean {
+    const tokens = breakStringIntoHourMinutes(str);
+    if (tokens.length < 2) {
+        return false;
+    }
+
+    const hour = Number(tokens[0]);
+    const minutes = Number(tokens[1]);
+
+    if (isNaN(hour) || isNaN(minutes)) {
+        return false;
+    }
+
+    return (hour >= 0 && hour <= 24) && (minutes >= 0 && minutes <= 60);
+}
+
+function breakStringIntoHourMinutes(str: string): string[] {
+    return str.split(':');
+}
+
